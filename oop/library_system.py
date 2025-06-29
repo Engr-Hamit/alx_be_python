40b06da2dc3e6cbb5c1930b4d80db7accfed6df0
Eng-Hamit <hamitgadar05@gmail.com>
2025-06-29 20:48:01 +0200

    Add __str__ methods to Book, EBook, and PrintBook for proper output

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,9 +1,11 @@
+# library_system.py
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_description(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +14,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_description(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +23,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_description(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -34,5 +36,5 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_description())
+            print(book)  # This uses the __str__ method of each book
 
